2b66852a8d518ca7c06f90097a680790affb75fe
andreahanzel <trky.andrea@gmail.com>
2025-01-21 15:03:45 -0800

    Implemented dynamic inventory classification pages with consistent styling and fixed layout issues for varying item counts

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const path = require("path")
 const app = express();
 const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController"); // Added this line to import the baseController
+const inventoryRoute = require("./routes/inventoryRoute"); // Added this line to import the inventoryRoute file
+
 
 /* **********************************
  * View Engine and Templates
@@ -27,11 +30,12 @@ app.use(express.static(path.join(__dirname, "public")));
 /* ***********************
  * Routes
  *************************/
+// Updated the Index route to use the baseController method
+app.get("/", baseController.buildHome); // Changed this line to call the buildHome method
 app.use(static);
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
+// Inventory routes
+app.use("/inv", inventoryRoute); // Added this line to use the inventoryRoute for "/inv" routes
 
 /* ***********************
  * Local Server Information
